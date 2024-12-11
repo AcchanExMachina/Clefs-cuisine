577ac19b1e904280be140d2373221b4aef4dbd9c
Isiphebe <38988153+AcchanExMachina@users.noreply.github.com>
2024-12-11 09:38:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Définir les prénoms et numéros
-let Samir = ["Samir", "0769178438"];
-let Mamadou = ["Mamadou", "0615710137"];
-let Doriane = ["Doriane", "0784735326"];
-let Cedric = ["Cédric", "0696050648"];
-let Maxime = ["Maxime", "0695373005"];
-let Maher = ["Maher", "0641291747"];
-let Bafode = ["Bafodé", "0789902163"];
+// Définir les prénoms, numéros et numéros de chambre
+let Samir = ["Samir", "0769178438", "332"];
+let Mamadou = ["Mamadou", "0615710137", "?"];
+let Doriane = ["Doriane", "0784735326", "?"];
+let Cedric = ["Cédric", "0696050648", "?"];
+let Maxime = ["Maxime", "0695373005", "?"];
+let Maher = ["Maher", "0641291747", "?"];
+let Bafode = ["Bafodé", "0789902163", "?"];
 
 // Ajouter les éléments dans le tableau principal
 let keysHolder = [Samir, Mamadou, Doriane, Cedric, Maxime, Maher, Bafode];
@@ -14,15 +14,16 @@ let keysHolder = [Samir, Mamadou, Doriane, Cedric, Maxime, Maher, Bafode];
 function updateKeysHolder(index) {
     // Vérifie si l'index est valide
     if (index >= 0 && index < keysHolder.length) {
-        
         let selectedKeyHolder = keysHolder[index];
-        // Modifier le contenu des balises
-        document.getElementById("forename").innerText = selectedKeyHolder[0];
-        document.getElementById("number").innerText = selectedKeyHolder[1];
+
+        // Modifier le contenu des balises correspondantes
+        document.querySelectorAll(".info")[0].innerText = selectedKeyHolder[0]; // Nom
+        document.querySelectorAll(".info")[1].innerText = selectedKeyHolder[1]; // Numéro de téléphone
+        document.querySelectorAll(".info")[2].innerText = selectedKeyHolder[2]; // Numéro de chambre
     } else {
         console.error("Index invalide.");
     }
 }
 
-console.log(updateKeysHolder(1));
-updateKeysHolder(1);
+// Exemple d'utilisation : mettre à jour avec le premier élément (Samir)
+updateKeysHolder(0);
